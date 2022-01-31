2b476c006ca63d134b4d9c794696842074e2644d
webdev <liuzhemin0305@gmail.com>
2022-01-31 19:30:05 +0900

    update config for test

--- config.ts
+++ config.ts
@@ -8,8 +8,11 @@ type IConfig = {
 const config: IConfig = {
   decimals: 18,
   airdrop: {
-    "0xa33df9272c7edcdce4bf920f601fe5a00c38f75c": 1000,
-  },
+    "0xa33dF9272C7EdCdce4bf920f601FE5A00C38F75c": 100000,
+    "0x37112CB8E83B30B24bB39f453dcEE69f8cA61058": 100000,
+    "0x508f07c99802B93b1c1F66B7E88122C53fc0Ed96": 700000,
+    "0xCDAb1207081d70F15798bdbD677C90E6a3EfF7b6": 500000
+  }
 };
 
 // Export config
